6ecb91377f6ef8bbdfa6a53aa1521865f5e5339a
KJG19 <kyleguenter19@gmail.com>
2025-01-21 13:44:05 -0700

    created landing-page component, routed it to open when starting local host

--- frontend/Sales-Analytic-Frontend/src/app/app.routes.ts
+++ frontend/Sales-Analytic-Frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : 'landing-page', component : LandingPageComponent}
+
+];
