b8271104e264f098633bda8bb99f5a44dd233566
ConDavison1 <c.davison18@gmail.com>
2025-01-22 12:11:07 -0700

    Completed the log in functionality and enabling backend connection

--- frontend/Sales-Analytic-Frontend/src/app/login/login.component.ts
+++ frontend/Sales-Analytic-Frontend/src/app/login/login.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  message: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onLogin() {
+    const payload = { username: this.username, password: this.password };
+
+    this.http.post('http://localhost:5000/login', payload).subscribe(
+      (response: any) => {
+        
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/landing-page']);
+      },
+      (error) => {
+        
+        this.message = error.error.message || 'Login failed!';
+      }
+    );
+  }
 }
