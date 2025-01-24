1f1814cb2029d2502ec2acf0b20d63c0e05ed7fc
Logan <logan.buye@edu.sait.ca>
2025-01-24 13:46:24 -0700

    Created revenue component

---
+++ frontend/Sales-Analytic-Frontend/src/app/revenue-page/revenue-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-revenue-page',
+  imports: [],
+  templateUrl: './revenue-page.component.html',
+  styleUrl: './revenue-page.component.css'
+})
+export class RevenuePageComponent {
+
+}
