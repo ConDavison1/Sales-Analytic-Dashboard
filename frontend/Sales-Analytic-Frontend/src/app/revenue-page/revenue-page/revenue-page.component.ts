9145de07f7531c4ea6ffdf926c9c050bb14c034e
Logan <logan.buye@edu.sait.ca>
2025-01-28 16:34:31 -0700

    Created Header and Sidebar for Revenue Page

--- frontend/Sales-Analytic-Frontend/src/app/revenue-page/revenue-page/revenue-page.component.ts
+++ frontend/Sales-Analytic-Frontend/src/app/revenue-page/revenue-page/revenue-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-revenue-page',
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent],
+  standalone: true,
   templateUrl: './revenue-page.component.html',
   styleUrl: './revenue-page.component.css'
 })
