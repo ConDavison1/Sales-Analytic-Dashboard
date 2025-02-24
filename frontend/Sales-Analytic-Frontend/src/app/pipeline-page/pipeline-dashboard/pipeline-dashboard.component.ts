04085b0c56e1aa7575257a842b800a464ff484fd
logan-buye <logan.buye@edu.sait.ca>
2025-02-23 21:03:43 -0700

    Created Pipeline components

---
+++ frontend/Sales-Analytic-Frontend/src/app/pipeline-page/pipeline-dashboard/pipeline-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipeline-dashboard',
+  imports: [],
+  templateUrl: './pipeline-dashboard.component.html',
+  styleUrl: './pipeline-dashboard.component.css'
+})
+export class PipelineDashboardComponent {
+
+}
---
+++ frontend/Sales-Analytic-Frontend/src/app/pipeline-page/pipeline-page/pipeline-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipeline-page',
+  imports: [],
+  templateUrl: './pipeline-page.component.html',
+  styleUrl: './pipeline-page.component.css'
+})
+export class PipelinePageComponent {
+
+}
