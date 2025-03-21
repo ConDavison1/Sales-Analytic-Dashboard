22fc99bf1b2e0673b64da00df61eaee7fafc3ed7
logan-buye <logan.buye@edu.sait.ca>
2025-03-20 19:19:11 -0600

    Created settings page component

---
+++ frontend/Sales-Analytic-Frontend/src/app/settings-page/settings-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-page',
+  imports: [],
+  templateUrl: './settings-page.component.html',
+  styleUrl: './settings-page.component.css'
+})
+export class SettingsPageComponent {
+  // settings ideas, light / dark mode, change password, change email, high contrast mode, time zone, default export preference
+}
